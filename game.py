84b7ba3b827f5a62df4225d371691f28f5e33a1f
Maksim Michailov <maximimpulse@mail>
2023-05-02 11:41:16 +0300

    game.py

--- game.py
+++ game.py
@@ -8,6 +8,68 @@ SCREEN_TITLE = "Далекий космос"
 CHARACTER_SCALING = 0.6
 TILE_SCALING = 0.5
 MOVEMENT_SPEED = 5
+class QuitButton(arcade.gui.UIFlatButton):
+    def on_click(self, event: arcade.gui.UIOnClickEvent):
+        arcade.exit()
+class StartView(arcade.View):
+    
+    def __init__(self):
+        
+        super().__init__()
+
+        self.manager = arcade.gui.UIManager()
+        self.manager.enable()
+
+       
+        arcade.set_background_color(arcade.color.DARK_BLUE_GRAY)
+
+        
+       
+        self.v_box = arcade.gui.UIBoxLayout()
+        
+        settings_button = arcade.gui.UIFlatButton(text="Начать игру", width=200)
+        self.v_box.add(settings_button.with_space_around(bottom=20))
+        settings_button.on_click = self.on_click_launch
+        
+        
+        
+        
+        
+        quit_button = QuitButton(text="Выход", width=200)
+        self.v_box.add(quit_button)
+        open_message_box_button = arcade.gui.UITextureButton(widht=200,hight=200,texture=arcade.load_texture(":resources:images/items/flagRed2.png"))
+        open_message_box_button.on_click = self.on_click_open
+        self.v_box.add(open_message_box_button)
+        self.manager.add(
+            arcade.gui.UIAnchorWidget(
+                anchor_x="center_x",
+                anchor_y="center_y",
+                child=self.v_box)
+        )
+    def on_click_open(self, event: arcade.gui.UIOnClickEvent):
+                message_box = arcade.gui.UIMessageBox(
+                width=300,
+                height=200,
+                message_text=(
+                "Добро пожаловать в игру 'Далекий Космос'. Чтобы двигаться используйте стрелочки, а чтобы стрелять используйте пробел"
+             ),
+                callback=self.on_message_box_close,
+                buttons=["Ok"]
+            )
+                self.manager.add(message_box)
+    def on_click_launch(self, event: arcade.gui.UIOnClickEvent):
+        game_view=GameView()
+        game_view.setup()
+        self.window.show_view(game_view)
+        
+        
+    def on_draw(self):
+        self.clear()
+        self.manager.draw()
+
+    def on_message_box_close(self, button_text):
+        print(f"User pressed {button_text}.")
+
 class TurningSprite(arcade.Sprite):
     def update(self):
         super().update()
@@ -141,9 +203,8 @@ class GameView(arcade.View):
 
 def main():
     window = arcade.Window(SCREEN_WIDTH,SCREEN_HEIGHT,SCREEN_TITLE)
-    start_view=GameView()
+    start_view=StartView()
     window.show_view(start_view)
-    start_view.setup()
     arcade.run()
 
 
